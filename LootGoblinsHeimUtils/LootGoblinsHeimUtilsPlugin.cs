ec5ac91061f962785aeb20ffd62f9c6b07ce9f55
okeanz <stargazer.lxfly@gmail.com>
2023-12-24 00:25:26 +0300

    Init v3

--- LootGoblinsHeimUtils/LootGoblinsHeimUtilsPlugin.cs
+++ LootGoblinsHeimUtils/LootGoblinsHeimUtilsPlugin.cs
@@ -1,9 +1,13 @@
-﻿using BepInEx;
+﻿using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+using Jotunn.Configs;
 using Jotunn.Entities;
 using Jotunn.Managers;
 using Jotunn.Utils;
+using UnityEngine;
 
-namespace JotunnModStub
+namespace LootGoblinsUtils
 {
     [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
@@ -14,13 +18,43 @@ namespace JotunnModStub
         public const string PluginName = "LootGoblinsHeimUtils";
         public const string PluginVersion = "0.0.1";
         
+        private readonly Harmony _harmony = new(PluginGuid);
+        
         public static CustomLocalization Localization = LocalizationManager.Instance.GetLocalization();
 
         private void Awake()
         {
             Jotunn.Logger.LogInfo("LootGoblinsHeimUtilsPlugin has landed");
             
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+
+            PrefabManager.OnVanillaPrefabsAvailable += AddBushes;
+
+        }
+
+        private void AddBushes()
+        {
+            Jotunn.Logger.LogWarning("------------ AddBushes start ------------");
             
+            var bushPiece = PieceManager.Instance.GetPiece("RaspberryBush");
+            Jotunn.Logger.LogWarning($"bushPiece found = {bushPiece != null}");
+            
+            var fermentorPiece = PieceManager.Instance.GetPiece("fermentor");
+            
+            Jotunn.Logger.LogWarning($"fermentorPiece found = {fermentorPiece != null}");
+
+            var pc = new PieceConfig();
+            pc.Name = "Малиновый куст";
+            pc.PieceTable = PieceTables.Cultivator;
+            pc.AddRequirement(new RequirementConfig("Raspberry", 1, 1));
+
+            PieceManager.Instance.AddPiece(new CustomPiece("RaspberryBush_LG", "fermenter", pc));
+                
+                
+            Jotunn.Logger.LogWarning("------------ AddBushes end ------------");
+
+            PrefabManager.OnVanillaPrefabsAvailable -= AddBushes;
         }
     }
 }
