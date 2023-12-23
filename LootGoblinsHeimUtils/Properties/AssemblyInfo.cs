0d656b6fb45a61c492b79dfaae9531f1362faf1c
okeanz <stargazer.lxfly@gmail.com>
2023-12-23 13:45:34 +0300

    Init v2

--- LootGoblinsHeimUtils/Properties/AssemblyInfo.cs
+++ LootGoblinsHeimUtils/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JotunnModStub")]
+[assembly: AssemblyTitle("LootGoblinsHeimUtils")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JotunnModStub")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyProduct("LootGoblinsHeimUtils")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
